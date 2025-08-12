9ff760f6b1303cf480c6f21b8b9363dbbbfcc6d0
Milan Stuhlsatz <milan.stuhlsatz@gmail.com>
2025-08-12 05:57:04 +0200

    Update Plant_Monitor.ino
    Set to MIT Licens

--- Plant_Monitor/Plant_Monitor.ino
+++ Plant_Monitor/Plant_Monitor.ino
@@ -1,7 +1,7 @@
 // Author: Milan Stuhlsatz
 // Date: 12.08.2025
 // Versiom: ALPHA
-// Licens: All rights reserved
+// Licens: MIT
 
 /************************************************************************/
 /** Do NOT edit anything beyond this point (unless u know what you do) **/
