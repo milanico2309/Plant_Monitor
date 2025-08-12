cacde026d72df58b1135f53ca19f445b3f989e80
Milan Stuhlsatz <milan.stuhlsatz@gmail.com>
2025-08-12 02:31:11 +0200

    Create Plant_Monitor.ino

---
+++ Plant_Monitor/Plant_Monitor.ino
@@ -0,0 +1,27 @@
+// Author: Milan Stuhlsatz
+// Date: 12.08.2025
+// Versiom: ALPHA
+// Licens: All rights reserved
+
+/************************************************************************/
+/** Do NOT edit anything beyond this point (unless u know what you do) **/
+/************************************************************************/
+void setup() {
+  
+      initPins();
+
+    //initialize output
+    initSerial();
+    initLED();
+    initDisplay();
+
+    //Initialize memory
+    initIDArray();
+    initDataArray();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
