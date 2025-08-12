291bedd27277a422a87a33249454463491534dd8
Milan Stuhlsatz <milan.stuhlsatz@gmail.com>
2025-08-12 05:50:29 +0200

    add some functionality but unfinished
    adding several things:
    - main loop logic
    - setup/init
    
    missing:
    - config
    - output to serial and display
    - git stuff like README

--- Plant_Monitor/const.h
+++ Plant_Monitor/const.h
@@ -5,19 +5,19 @@
 #ifndef SOILMANAGER_CONST_H
 #define SOILMANAGER_CONST_H
 
-enum sensorDataArrayOffsets{
- VALUE = 0,
- PIN = 1,
- CALIBRATED_MIN = 2,
- CALIBRATED_MAX = 3
+enum sensorDataArrayOffsets {
+  VALUE = 0,
+  PIN = 1,
+  CALIBRATED_MIN = 2,
+  CALIBRATED_MAX = 3
 };
 
- enum TimeConversionFactorMillis{
+enum TimeConversionFactorMillis {
   F_INSTANT = 0,
   F_MILLIS = 1,
   F_SECONDS = 1000,
   F_MINUTES = 60000,
   F_HOURS = 3600000
- };
+};
 
-#endif //SOILMANAGER_CONST_H
+#endif  //SOILMANAGER_CONST_H
---
+++ Plant_Monitor/types.h
@@ -0,0 +1,9 @@
+/************************************************************************/
+/** Do NOT edit anything beyond this point (unless u know what you do) **/
+/************************************************************************/
+
+struct SensorValue{
+  int id;
+  char friendlyName[21];
+  int value;
+}
